4b488c6236bcd28c90242c3a49f05ee429c0403d
Loren Rivers <loren.rivers@hotmail.co.uk>
2024-02-26 10:20:28 +0000

    more pages

---
+++ src/app/another-cool-page/page.js
@@ -0,0 +1,7 @@
+export default function AnotherPage() {
+  return (
+    <div>
+      <h2>WOW... Another Page!!</h2>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import AnotherPage from "./another-cool-page/page";
 
 export default function Home() {
   return (
