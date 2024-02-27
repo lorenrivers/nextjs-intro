ee1f72459b2b0fe184ebe753e757dfbdf7635507
Loren Rivers <loren.rivers@hotmail.co.uk>
2024-02-27 13:18:46 +0000

    adding metadata

--- src/app/another-cool-page/[id]/page.js
+++ src/app/another-cool-page/[id]/page.js
@@ -1,3 +1,27 @@
+export async function generateMetaData({ params }) {
+  const result = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
+  const post = await result.json();
+
+  return {
+    title: `${post.title}`,
+    description: `This is an article about ${post.title}`,
+  };
+}
+
+//SQL example
+// export async function generateMetadata({ params, searchParams }, parent) {
+
+//   const { rows: posts } =
+//     await sql`SELECT * FROM posts WHERE posts.Id = ${params.id}`;
+//   const post = posts[0]; // get the first one
+
+//   return {
+//     title: post.title,
+//   };
+// }
+
 export default async function Page({ params }) {
   console.log(params);
   const response = await fetch(
